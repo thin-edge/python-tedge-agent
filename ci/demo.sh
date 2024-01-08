b7cfdec33bc4ed0d6fbe8a0c89d70b4d76e46f6b
Reuben Miller <reuben.d.miller@gmail.com>
2024-01-08 19:26:51 +0100

    ci: wait for bootstrap container to be ready

--- ci/demo.sh
+++ ci/demo.sh
@@ -6,8 +6,28 @@ just prepare-up
 just up --build=false >/dev/null 2>&1 &
 UP_PID=$!
 
-sleep 5
-just bootstrap "$DEVICE_ID" </dev/null
+# Wait for bootstrap service to 
+COUNTER=1
+TIMEOUT=60
+BOOTSTRAP=1
+if [ "$(docker compose ps tedge -a --format '{{.State}}')" != "running" ]; then
+    while [ "$(docker compose ps bootstrap -a --format '{{.State}}')" != "running" ]; do
+        if [ "$COUNTER" -ge "$TIMEOUT" ]; then
+            echo "Timed out waiting for bootstrap service to be ready" >&2
+            exit 1
+        fi
+        echo "bootstrap service not running...attempt $COUNTER" >&2
+        sleep 1
+        COUNTER=$((COUNTER + 1))
+    done
+else
+    echo "tedge service is already running (bootstrapping was probably already done)" >&2
+    BOOTSTRAP=0
+fi
+
+if [ "$BOOTSTRAP" = 1 ]; then
+    just bootstrap "$DEVICE_ID" </dev/null
+fi
 
 # Wait until bootstrap is ready
 wait "$UP_PID"
